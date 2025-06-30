29651b14cdde393bf62a657b6192771e328e8a33
arkham <jesm430@gmail.com>
2025-06-30 00:01:24 -0700

    Solucion al RQ-011

--- enemigos/policia/policia.gd
+++ enemigos/policia/policia.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 @export var bala_escena: PackedScene
 @onready var spawn_point = $SpawnPoint
@@ -12,3 +12,8 @@ func _on_Timer_timeout():
 	var nueva_bala = bala_escena.instantiate()
 	nueva_bala.position = spawn_point.global_position
 	get_tree().current_scene.add_child(nueva_bala)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		queue_free()
+		get_tree().current_scene.get_node("MensajeFinal").visible = true
